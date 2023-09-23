c92da4150d0c5b4908cf6cdbf52a91dd746a75f5
Hugo <hugokorteapple@gmail.com>
2023-09-23 23:01:51 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ import { init } from './commands/init.js';
 const program = new Command();
 
 program.version('0.0.1');
-
 program.command('initialize').alias('init').description('initialize capkit').action(init);
 program.parse(process.argv);
